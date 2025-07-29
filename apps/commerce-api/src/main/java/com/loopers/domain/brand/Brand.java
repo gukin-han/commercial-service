147bf42443914533e86ec530051992fb2bccb16a
Gukin Han <gukin.dev@gmail.com>
2025-07-29 22:08:27 +0900

    feat : 브랜드 도메인

---
+++ apps/commerce-api/src/main/java/com/loopers/domain/brand/Brand.java
@@ -0,0 +1,12 @@
+package com.loopers.domain.brand;
+
+import com.loopers.domain.BaseEntity;
+
+public class Brand extends BaseEntity {
+
+    private String name;
+
+    public Brand(String name) {
+        this.name = name;
+    }
+}
