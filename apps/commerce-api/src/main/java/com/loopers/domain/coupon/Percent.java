c207110e25b45f6fabde7281bf6fe357f05132cc
gukin-han <gukin.dev@gmail.com>
2025-08-10 13:12:37 +0900

    test : 퍼센트 vo 생성시 NaN값 대응

--- apps/commerce-api/src/main/java/com/loopers/domain/coupon/Percent.java
+++ apps/commerce-api/src/main/java/com/loopers/domain/coupon/Percent.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import java.util.Objects;
 
 @Getter
 @Embeddable
+@EqualsAndHashCode
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Percent {
 
@@ -19,11 +21,17 @@ public class Percent {
 
     private Percent(Double value) {
         if (value == null) {
-            throw new IllegalArgumentException("Percent value cannot be null");
+            throw new IllegalArgumentException("null은 할인율은 될 수 없습니다.");
         }
+
+        if (value.isNaN()) {
+            throw new IllegalArgumentException("NaN은 할인율은 될 수 없습니다.");
+        }
+
         if (value <= 0.0 || value > 1.0) {
             throw new IllegalArgumentException("할인율은 0보다 크고 1 이하이어야 합니다.");
         }
+
         this.value = value;
     }
 
@@ -31,16 +39,4 @@ public class Percent {
         return new Percent(value);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Percent percent = (Percent) o;
-        return Objects.equals(value, percent.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
-    }
 }
